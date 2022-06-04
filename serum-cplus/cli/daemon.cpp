45d12341edb9cff3041a964e20534ef8affaaa4a
d-laslo <ice_bear@ukr.net>
2022-06-04 23:31:26 +0300

    daemon server added

--- serum-cplus/cli/daemon.cpp
+++ serum-cplus/cli/daemon.cpp
@@ -26,10 +26,10 @@ void signal_callback_handler(int signum) {
     go_exit = true;
 }
 
-static void skeleton_daemon();
+static void daemon();
 
 int main(int argc, char **argv) {
-    skeleton_daemon();
+    daemon();
 
     printf("Hello FixServer\n");
 
@@ -67,57 +67,32 @@ int main(int argc, char **argv) {
         }
     });
 
+    closelog();
 }
    
-static void skeleton_daemon()
+static void daemon()
 {
     pid_t pid;
     
     /* Fork off the parent process */
     pid = fork();
-    
-    /* An error occurred */
     if (pid < 0)
         exit(EXIT_FAILURE);
-    
-     /* Success: Let the parent terminate */
     if (pid > 0)
         exit(EXIT_SUCCESS);
-    
-    /* On success: The child process becomes session leader */
-    if (setsid() < 0)
+    if (setsid() < 0) {
+        printf("Could not generate session ID for child process");
         exit(EXIT_FAILURE);
-    
-    /* Catch, ignore and handle signals */
-    /*TODO: Implement a working signal handler */
-    signal(SIGCHLD, SIG_IGN);
-    signal(SIGHUP, SIG_IGN);
-    
-    /* Fork off for the second time*/
-    pid = fork();
-    
-    /* An error occurred */
-    if (pid < 0)
-        exit(EXIT_FAILURE);
-    
-    /* Success: Let the parent terminate */
-    if (pid > 0)
-        exit(EXIT_SUCCESS);
-    
-    /* Set new file permissions */
-    umask(0);
-    
-    /* Change the working directory to the root directory */
-    /* or another appropriated directory */
-    chdir("/");
-    
-    /* Close all open file descriptors */
+    }
+
+    umask(0);    
+    chdir("./");
+
     int x;
     for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
     {
         close (x);
     }
-    
-    /* Open the log file */
-    openlog ("firstdaemon", LOG_PID, LOG_DAEMON);
+
+    openlog ("FixServerLog", LOG_PID, LOG_DAEMON);
 }
