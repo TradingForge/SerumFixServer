b045546ba638d05627d154daa9eb11b232d23a17
d-laslo <ice_bear@ukr.net>
2022-06-11 19:29:07 +0300

    daemon fixed

--- serum-cplus/cli/daemon.cpp
+++ serum-cplus/cli/daemon.cpp
@@ -9,6 +9,7 @@
 
 
 #include <iostream>
+#include <functional>
 #include <string>
 #include <vector>
 #include <chrono>
@@ -17,73 +18,69 @@
 #include <sharedlib/include/Logger.h>
 #include <serum/SERUM_Data_session.hpp>
 
-using namespace std;
-bool go_exit = false;
+volatile bool go_exit = false;
 
-// Define the function to be called when ctrl-c (SIGINT) is sent to process
-void signal_callback_handler(int signum) {
-    std::cout << "Caught ctrl+c signal " << signum << std::endl;
-    // Terminate program
+void sigint_handler(int sig)
+{
     go_exit = true;
 }
 
-static void daemon();
+static void daemon(std::function<void()>);
 
 int main(int argc, char **argv) {
-    daemon();
+    daemon([](){
+        printf("Hello FixServer\n");
 
-    printf("Hello FixServer\n");
+        std::string conf_file = "hf_server.xml";
+        std::unique_ptr<FIX8::ServerSessionBase> ms(
+                new FIX8::ServerSession<SERUM_Data_session>(FIX8::SERUM_Data::ctx(), conf_file, "SERUM_MD"));
 
-    std::string conf_file = "hf_server.xml";
-    std::unique_ptr<FIX8::ServerSessionBase> ms(
-            new FIX8::ServerSession<SERUM_Data_session>(FIX8::SERUM_Data::ctx(), conf_file, "SERUM_MD"));
+        typedef std::shared_ptr<FIX8::SessionInstanceBase> ClientSession;
+        std::vector<ClientSession> sessions;
 
-    typedef std::shared_ptr<FIX8::SessionInstanceBase> ClientSession;
-    std::vector<ClientSession> sessions;
-
-    while(!go_exit)
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
-        if(!sessions.empty()) {
-            int count = sessions.size();
-            sessions.erase(
-                    std::remove_if(sessions.begin(), sessions.end(), [](ClientSession &sess) {
-                        if (sess->session_ptr()->is_shutdown())
-                            printf("Erase session: %s\n", sess->session_ptr()->get_sid().get_id().c_str());
-                        return sess->session_ptr()->is_shutdown();
-                    }),
-                    sessions.end()
-            );
-            if(count != sessions.size()) {
-                printf("Session removed, count= %d\n", (int) sessions.size());
+        while(!go_exit)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+            if(!sessions.empty()) {
+                int count = sessions.size();
+                sessions.erase(
+                        std::remove_if(sessions.begin(), sessions.end(), [](ClientSession &sess) {
+                            if (sess->session_ptr()->is_shutdown())
+                                printf("Erase session: %s\n", sess->session_ptr()->get_sid().get_id().c_str());
+                            return sess->session_ptr()->is_shutdown();
+                        }),
+                        sessions.end()
+                );
+                if(count != sessions.size()) {
+                    printf("Session removed, count= %d\n", (int) sessions.size());
+                }
+            }
+            if (ms->poll())
+            {
+                std::shared_ptr<FIX8::SessionInstanceBase> inst(ms->create_server_instance());
+                sessions.push_back(inst);
+                printf("Session added, count= %d\n", (int)sessions.size());
+                //inst->session_ptr()->control() |= FIX8::Session::print;
+                //FIX8::GlobalLogger::log("global_logger");
+                //const FIX8::ProcessModel pm(ms->get_process_model(ms->_ses));
+                inst->start(false);
             }
         }
-        if (ms->poll())
-        {
-            std::shared_ptr<FIX8::SessionInstanceBase> inst(ms->create_server_instance());
-            sessions.push_back(inst);
-            printf("Session added, count= %d\n", (int)sessions.size());
-            //inst->session_ptr()->control() |= FIX8::Session::print;
-            //FIX8::GlobalLogger::log("global_logger");
-            //const FIX8::ProcessModel pm(ms->get_process_model(ms->_ses));
-            inst->start(false);
-        }
-    }
 
-    //  deinitialize all services
-    std::for_each(sessions.begin(),sessions.end(),[](ClientSession& sess)
-    {
-        if(!sess->session_ptr()->is_shutdown())
+        //  deinitialize all services
+        std::for_each(sessions.begin(),sessions.end(),[](ClientSession& sess)
         {
-            sess->session_ptr()->stop();
-        }
+            if(!sess->session_ptr()->is_shutdown())
+            {
+                sess->session_ptr()->stop();
+            }
+        });
+        std::this_thread::sleep_for(std::chrono::milliseconds(250));
     });
-    std::this_thread::sleep_for(std::chrono::milliseconds(250));
 
-    closelog();
 }
    
-static void daemon()
+static void daemon(std::function<void()> payload)
 {
     pid_t pid;
     
@@ -108,4 +105,7 @@ static void daemon()
     }
 
     openlog ("FixServerLog", LOG_PID, LOG_DAEMON);
+    signal(SIGINT, sigint_handler);
+    payload();
+    closelog();
 }
