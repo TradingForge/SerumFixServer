2769827a23c903e20c6200625aa4e6f183ffebdc
root <root@DESKTOP-1CPNO75.localdomain>
2023-02-19 11:31:36 +0200

    add real order channel template

---
+++ serum-cplus/serum/ConsoleLogger.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Zay on 2/19/2023.
+//
+
+#include "ConsoleLogger.h"
---
+++ serum-cplus/serum/ConsoleLogger.h
@@ -0,0 +1,16 @@
+//
+// Created by Zay on 2/19/2023.
+//
+
+#ifndef SERUM_FIX_SERVER2_CONSOLELOGGER_H
+#define SERUM_FIX_SERVER2_CONSOLELOGGER_H
+
+
+
+class ConsoleLogger {
+
+};
+
+
+
+#endif //SERUM_FIX_SERVER2_CONSOLELOGGER_H
