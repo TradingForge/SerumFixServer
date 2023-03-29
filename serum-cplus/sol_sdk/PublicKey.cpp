79df1ea09b2309d57a72c70c8cf84ab059758100
d-laslo <ice_bear@ukr.net>
2023-03-29 19:12:14 +0300

    added search for the associated key not on the curve

--- serum-cplus/sol_sdk/PublicKey.cpp
+++ serum-cplus/sol_sdk/PublicKey.cpp
@@ -1,14 +1,33 @@
 #include "PublicKey.hpp"
 
+#include <cryptopp/default.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/filters.h>
+#include <cryptopp/sha.h>
+#include <cryptopp/ecp.h>
+#include <cryptopp/asn.h>
+#include <cryptopp/eccrypto.h>
+// #include <cryptopp/dlies.h>
+
 namespace solana
 {
-    PublicKey::PublicKey() : key_str(), key_b(bytes())
-    {}
+    PublicKey::PublicKey() : key_b(SIZE_PUBKEY, 0)
+    {
+
+        key_str= _to_base58(key_b);
+    }
 
     PublicKey::PublicKey(const string& key) : key_str(key), key_b(_from_base58(key_str))
-    {}
+    {
+        auto k = _from_base58(key_str);
+        if (k.size() != SIZE_PUBKEY) {
+            throw -1;
+        }
 
-    PublicKey::PublicKey(const bytes& key)
+        key_b = k;
+    }
+
+    PublicKey::PublicKey(const bytes& key) : key_b(key)
     {
         if (key.size() != SIZE_PUBKEY) {
             throw -1;
@@ -95,5 +114,63 @@ namespace solana
         key.key_str = "";
 
         return *this;
+    }
+
+
+    std::tuple<PublicKey, PublicKey::byte> PublicKey::find_public_key(const std::list<PublicKey>& seeds, const PublicKey& program_id)
+    {
+        const auto &program_id_bytes = program_id.key_b;
+        std::list<bytes> seeds_bytes;
+        for ( const auto& s : seeds)
+            seeds_bytes.push_back(s.key_b);
+
+        byte seed_bump = UINT8_MAX;
+
+        bool is_curve_point = false;
+        bytes hash;
+        do{
+            hash = try_find_public_key(program_id_bytes, seed_bump, seeds_bytes);
+            --seed_bump;
+            // is_curve_point = bytes_are_curve_point(hash);
+        } while (seed_bump > 0 && is_curve_point);
+
+        if (is_curve_point)
+            return std::tuple<PublicKey, byte>(PublicKey(), 0);
+        
+        return std::tuple<PublicKey, byte>(PublicKey(hash), ++seed_bump);
+    }
+
+    // bool bytes_are_curve_point(const CryptoPP::byte* bytes, size_t len) {
+    //     // Validate that the given bytes form a valid curve point
+    //     // This implementation assumes that the input is a valid ECPoint
+    //     CryptoPP::ECIES<CryptoPP::ECP>::Decryptor d;
+
+    //     CryptoPP::OID curve_oid("1.3.132.0.10");
+    //     d.AccessKey().AccessGroupParameters().Initialize(CryptoPP::OID("1.3.132.0.10"));
+    //     CryptoPP::ECP ec;
+    //     CryptoPP::ECP::Point p;
+    //     ec.DecodePoint(p, bytes, len);
+    //     // ec.DecodePoint(bytes, len);
+    //     return ec.VerifyPoint(p);
+    //     // CryptoPP::ECP::Point p;
+    //     // p.Decode(bytes, len);
+    //     // return p.IsValidElement();
+    // }
+
+    PublicKey::bytes PublicKey::try_find_public_key(const bytes program_id, byte seed_bump, const std::list<bytes>& seeds)
+    {
+        CryptoPP::SHA256 hasher;
+
+        for (auto seed : seeds) {
+            hasher.Update(seed.data(), seed.size());
+        }
+        hasher.Update(&seed_bump, 1);
+        hasher.Update(program_id.data(), program_id.size());
+        hasher.Update((byte*)PDA_MARKER, sizeof(PDA_MARKER) - 1);
+
+        bytes digest(CryptoPP::SHA256::DIGESTSIZE);
+        hasher.Final(digest.data());
+
+        return digest;
     }
 } // namespace solana
--- serum-cplus/sol_sdk/PublicKey.hpp
+++ serum-cplus/sol_sdk/PublicKey.hpp
@@ -1,11 +1,14 @@
 #pragma once
 #include <vector>
+#include <list>
 #include <string>
 #include <string.h>
 #include <base58/base58.h>
+#include <tuple>
 
 namespace solana
 {
+    #define PDA_MARKER "ProgramDerivedAddress"
     #define SIZE_PUBKEY 32
 
     class PublicKey 
@@ -20,6 +23,7 @@ namespace solana
 
         bytes _from_base58(const string&);
         string _to_base58(const bytes&);
+        static bytes try_find_public_key(const bytes program_id, byte seed_bump, const std::list<bytes>& seeds);
     public:
         PublicKey();
         PublicKey(const string&);
@@ -28,7 +32,7 @@ namespace solana
         PublicKey(PublicKey&&);
         PublicKey(const byte[SIZE_PUBKEY]);
     
-
+        static std::tuple<PublicKey, byte> find_public_key(const std::list<PublicKey>& seeds, const PublicKey& program_id);
         void from_base58(const string&);
         const uint8_t* data() const {return key_b.data();}
         size_t size() const {return key_b.size();}
